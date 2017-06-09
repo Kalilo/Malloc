49439b39b838c19c138be07e77ba524e33e2f1dd
Kalilo <kalilohansmann@gmail.com>
2017-06-09 15:25:14 +0200

    Added more tests

--- testing/test_main00.c
+++ testing/test_main00.c
@@ -15,8 +15,19 @@
 
 int	main()
 {
-	printf("Attempting To malloc, and free 12bytes:\n");
+	printf("Attempting To malloc, and free 12 bytes:\n");
 	void	*test00 = malloc(12);
 	free(test00);
+	printf("\tSucess!\n");
+	printf("Attempting To malloc, and free 10 * 32 bytes:\n");
+	void	*test01[10];
+	for (int k = 0; k < 10; k++) {
+		test01[k] = malloc(32);
+		printf("allocated test01[%d]: %p\n", k, test01[k]);
+	}
+	for (int k = 0; k < 10; k++) {
+		printf("freeing test01[%d]: %p\n", k, test01[k]);
+		free(test01[k]);
+	}
 	printf("\tSucess!\n");
 }
