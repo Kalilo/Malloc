248c6e891b1da57f834c9f2e60bfbc74c3d5fc90
Kalilo <kalilohansmann@gmail.com>
2017-06-09 08:37:04 +0200

    added basic case handing for freeing tiny and small blocks

--- srcs/free.c
+++ srcs/free.c
@@ -14,23 +14,54 @@
 
 void	free_tiny_block(t_block_zone *block, void *ptr)
 {
-	t_tiny_list	*tiny;
+	t_tiny_list		*tiny;
+	t_block_zone	*parent_block;
 
 	tiny = (t_tiny_list *)(ptr - 1);
 	if (!tiny->used)
 		malloc_error_quit("Attempting to double free pointer");
 	tiny->used = 0;
 	block->active_members--;
+	if (!block->active_members && !block->next && block != g_zones.tiny_block)
+	{
+		parent_block = g_zones.tiny_block;
+		while (parent_block && parent_block->next != block)
+			parent_block = parent_block->next;
+		if (parent_block->next == block)
+		{
+			parent_block->next = NULL;
+			munmap(block, block->ps.size);
+		}
+	}
 }
 
 void	free_small_block(t_block_zone *block, void *ptr)
 {
-	//
+	t_small_list		*small;
+	t_block_zone	*parent_block;
+
+	small = (t_small_list *)(ptr - 2);
+	if (!small->used)
+		malloc_error_quit("Attempting to double free pointer");
+	small->used = 0;
+	block->active_members--;
+	if (!block->active_members && !block->next &&
+		block != g_zones.small_block)
+	{
+		parent_block = g_zones.small_block;
+		while (parent_block && parent_block->next != block)
+			parent_block = parent_block->next;
+		if (parent_block->next == block)
+		{
+			parent_block->next = NULL;
+			munmap(block, block->ps.size);
+		}
+	}
 }
 
 void	free_large_block(t_block_zone *block, void *ptr)
 {
-	//
+	t_block_zone	*parent_block;
 }
 
 void	free_all_blocks(void)
