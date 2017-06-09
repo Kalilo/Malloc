9afe1a5ffecd826338be8bed1fd32eda3659ce86
Kalilo <kalilohansmann@gmail.com>
2017-06-09 08:50:40 +0200

    added realloc prototype

---
+++ srcs/realloc.c
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   realloc.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: khansman <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/06/09 08:48:21 by khansman          #+#    #+#             */
+/*   Updated: 2017/06/09 08:48:22 by khansman         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/malloc.h"
+
+void			*realloc(void *ptr, size_t size)
+{
+	// find block, attempt to resize or malloc new block
+	return (NULL);
+}
