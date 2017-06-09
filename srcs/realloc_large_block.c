226a32cb81cda36703787322ac680c30e1bce939
Kalilo <kalilohansmann@gmail.com>
2017-06-09 10:00:38 +0200

    switched up logic

--- srcs/realloc_large_block.c
+++ srcs/realloc_large_block.c
@@ -14,33 +14,31 @@
 
 char	resize_large_block(t_block_zone *block, size_t size, t_page_size ps)
 {
-	t_block_zone	*parent_block;
-	t_block_zone	*new_block;
-
 	if (allocate_page(block, ps.size))
 	{
 		block->ps = ps;
 		return (1);
 	}
-	else
-	{
-		parent_block = g_zones.large_block;
-		while (parent_block->next && parent_block->next != block)
-			parent_block = parent_block->next;
-		FATAL_ERROR_RETURN((new_block = allocate_page(NULL, ps.size)) == NULL);
-		new_block->next = block->next;
-		new_block->ps = ps;
-		parent_block->next = new_block;
-		munmap(block, block->ps.size);
-	}
-	return (1);
+	return (0);
 }
 
-char	realloc_large_block(t_block_zone *block, size_t size)
+void	*realloc_large_block(t_block_zone *block, size_t size)
 {
 	t_page_size		ps;
+	t_block_zone	*parent_block;
+	t_block_zone	*new_block;
 
 	ps = round_to_pagesize(size + sizeof(t_block_zone));
-	SUCCESS_RETURN(ps.pages == block->ps.pages);
-	return (resize_large_block(block, size, ps));
+	if (ps.pages == block->ps.pages || resize_large_block(block, size, ps))
+		return (block);
+	parent_block = g_zones.large_block;
+	while (parent_block->next && parent_block->next != block)
+		parent_block = parent_block->next;
+	if ((new_block = allocate_page(NULL, ps.size)) == NULL)
+		return (NULL);
+	new_block->next = block->next;
+	new_block->ps = ps;
+	parent_block->next = new_block;
+	munmap(block, block->ps.size);
+	return (NULL);
 }
