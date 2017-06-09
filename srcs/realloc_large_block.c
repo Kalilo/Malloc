9d65f30e5d1e98d75c936c5669033f312f835ca1
Kalilo <kalilohansmann@gmail.com>
2017-06-09 09:52:11 +0200

    auto commit

--- srcs/realloc_large_block.c
+++ srcs/realloc_large_block.c
@@ -12,7 +12,44 @@
 
 #include "../includes/malloc.h"
 
+char	increase_large_block(t_block_zone *block, size_t size, t_page_size ps)
+{
+	t_block_zone	*parent_block;
+	t_block_zone	*new_block;
+
+	if (allocate_page(block, ps.size))
+	{
+		block->ps = ps;
+		return (1);
+	}
+	else
+	{
+		parent_block = g_zones.large_block;
+		while (parent_block->next && parent_block->next != block)
+			parent_block = parent_block->next;
+		FATAL_ERROR_RETURN((new_block = allocate_page(NULL, ps.size)) == NULL);
+		new_block->next = block->next;
+		new_block->ps = ps;
+		parent_block->next = new_block;
+		munmap(block, block->ps.size);
+	}
+	return (0);
+}
+
 char	realloc_large_block(t_block_zone *block, size_t size)
 {
-	//
+	t_page_size		ps;
+
+	ps = round_to_pagesize(size + sizeof(t_block_zone));
+	SUCCESS_RETURN(ps.pages == block->ps.pages);
+	if (ps.pages > block->ps.pages)
+	{
+		if (allocate_page(block, ps.size))
+		{
+			block->ps = ps;
+			return (1);
+		}
+	}
+	else
+	return (0);
 }
