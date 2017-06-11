036283b53e7914d1278754cccb72300c6777798f
Kalilo <kalilohansmann@gmail.com>
2017-06-11 10:56:44 +0200

    Updated output messages

--- testing/test_main00.c
+++ testing/test_main00.c
@@ -21,14 +21,14 @@ int	main()
 {
 	/* Test 00 */
 	printf("\nTest 00\n");
-	printf("Attempting To malloc, and free 12 bytes:\n");
+	printf("Attempting to malloc, and free 12 bytes:\n");
 	void	*test00 = malloc(12);
 	free(test00);
 	printf("\tSucess!\n");
 
 	/* Test 01 */
 	printf("\nTest 01\n");
-	printf("Attempting To malloc, and free 10 * 32 bytes:\n");
+	printf("Attempting to malloc, and free 10 * 32 bytes:\n");
 	void	*test01[10];
 	for (int k = 0; k < 10; k++) {
 		test01[k] = malloc(32);
@@ -42,14 +42,14 @@ int	main()
 
 	/* Test 02 */
 	printf("\nTest 02\n");
-	printf("Attempting To malloc, and free 255 bytes:\n");
+	printf("Attempting to malloc, and free 255 bytes:\n");
 	void	*test02 = malloc(255);
 	free(test02);
 	printf("\tSucess!\n");
 
 	/* Test 03 */
 	printf("\nTest 03\n");
-	printf("Attempting To malloc, and free 10 * 1024 bytes:\n");
+	printf("Attempting to malloc, and free 10 * 1024 bytes:\n");
 	void	*test03[10];
 	for (int k = 0; k < 10; k++) {
 		test03[k] = malloc(1024);
@@ -63,14 +63,14 @@ int	main()
 
 	/* Test 04 */
 	printf("\nTest 04\n");
-	printf("Attempting To malloc, and free 100000 bytes:\n");
+	printf("Attempting to malloc, and free 100000 bytes:\n");
 	void	*test04 = malloc(100000);
 	free(test04);
 	printf("\tSucess!\n");
 
 	/* Test 05 */
 	printf("\nTest 05\n");
-	printf("Attempting To malloc, and free 10 * 102400 bytes:\n");
+	printf("Attempting to malloc, and free 10 * 102400 bytes:\n");
 	void	*test05[10];
 	for (int k = 0; k < 10; k++) {
 		test05[k] = malloc(102400);
@@ -85,19 +85,19 @@ int	main()
 	/* Test 06 */
 	printf("\nTest 06\n");
 	void	*test06[1000];
-	printf("Attempting To malloc, and free 1000 * 1 byte:\n");
+	printf("Attempting to malloc, and free 1000 * 1 byte:\n");
 	for (int k = 0; k < 1000; k++)
 		test06[k] = malloc(1);
 	for (int k = 0; k < 1000; k++)
 		free(test06[k]);
 	printf("\tSucess!\n");
-	printf("Attempting To malloc, and free 1000 * 1 byte:\n");
+	printf("Attempting to malloc, and free 1000 * 1 byte:\n");
 	for (int k = 0; k < 1000; k++)
 		test06[k] = malloc(1);
 	for (int k = 0; k < 1000; k++)
 		free(test06[k]);
 	printf("\tSucess!\n");
-	printf("Attempting To malloc, and free 1000 * 8 byte:\n");
+	printf("Attempting to malloc, and free 1000 * 8 byte:\n");
 	for (int k = 0; k < 1000; k++)
 		test06[k] = malloc(8);
 	for (int k = 0; k < 1000; k++)
@@ -106,6 +106,7 @@ int	main()
 
 	/* Last Test */
 	printf("\nLast Test\n");
+	printf("Attempting to double free NULL\n");
 	free(NULL);
 	free(NULL);
 	printf("\tSucess!\n");
